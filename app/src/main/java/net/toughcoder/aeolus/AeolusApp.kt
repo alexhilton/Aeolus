b20edf2ea4e5ff031532ded8bbebd8d1d4e9f675
Alex Hilton <dragonhilton@gmail.com>
2023-09-26 19:30:56 +0800

    Adjust padding.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/AeolusApp.kt
+++ app/src/main/java/net/toughcoder/aeolus/AeolusApp.kt
@@ -1,20 +1,22 @@
 package net.toughcoder.aeolus
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import net.toughcoder.aeolus.ui.theme.AeolusTheme
 
 @Composable
 fun AeolusApp(modifier: Modifier = Modifier) {
     AeolusScreen(
-        modifier,
+        modifier.padding(6.dp),
         viewModel()
     )
 }
 
-@Preview
+@Preview(widthDp = 480)
 @Composable
 fun AeolusScreenPreview() {
     AeolusTheme {
