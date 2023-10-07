7df9c38e378de040714aba9beebcd46911b8f3ef
Alex Hilton <dragonhilton@gmail.com>
2023-10-07 15:19:09 +0800

    Let the app handles theme and background surface.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/AeolusApp.kt
+++ app/src/main/java/net/toughcoder/aeolus/AeolusApp.kt
@@ -1,19 +1,27 @@
 package net.toughcoder.aeolus
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import net.toughcoder.aeolus.ui.theme.AeolusTheme
 
 @Composable
 fun AeolusApp(modifier: Modifier = Modifier) {
-    AeolusScreen(
-        modifier,
-        viewModel()
-    )
+    AeolusTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = modifier,
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AeolusScreen(
+                modifier,
+                viewModel()
+            )
+        }
+    }
 }
 
 @Preview(widthDp = 480)
--- app/src/main/java/net/toughcoder/aeolus/MainActivity.kt
+++ app/src/main/java/net/toughcoder/aeolus/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,15 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AeolusTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AeolusApp(Modifier.fillMaxSize())
-                }
-            }
+            AeolusApp(Modifier.fillMaxWidth())
         }
     }
 }
