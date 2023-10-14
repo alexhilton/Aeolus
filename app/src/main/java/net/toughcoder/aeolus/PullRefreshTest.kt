fbbd258323c9aac70e88afba39f1750894fcff0a
Alex Hilton <dragonhilton@gmail.com>
2023-10-14 10:13:48 +0800

    Verify the PullRefresh.
    PullRefresh works perfectly inside of a Scaffold. It has nothing to do with Scaffold.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/PullRefreshTest.kt
+++ app/src/main/java/net/toughcoder/aeolus/PullRefreshTest.kt
@@ -2,13 +2,19 @@ package net.toughcoder.aeolus
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ListItem
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -19,10 +25,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextOverflow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun PullRefresh() {
     val refreshScope = rememberCoroutineScope()
@@ -38,19 +45,40 @@ fun PullRefresh() {
 
     val state = rememberPullRefreshState(refreshing, { refresh() })
 
-    Box(Modifier.pullRefresh(state)) {
-        LazyColumn(Modifier.fillMaxSize()) {
-            items(itemCount) {
-                ListItem { Text("Item #$it") }
-            }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    androidx.compose.material3.Text(
+                        text = "PullRefreshTest",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
         }
+    ) {
+        Box(
+            Modifier
+                .pullRefresh(state)
+                .padding(it.calculateTopPadding())) {
+            LazyColumn(Modifier.fillMaxSize()) {
+                items(itemCount) {
+                    ListItem { Text("Item #$it") }
+                }
+            }
 
-        PullRefreshIndicator(
-            refreshing = refreshing,
-            state = state,
-            modifier = Modifier.align(Alignment.TopCenter),
-            contentColor = colorResource(R.color.teal_700),
-            scale = true
-        )
+            PullRefreshIndicator(
+                refreshing = refreshing,
+                state = state,
+                modifier = Modifier.align(Alignment.TopCenter),
+                contentColor = colorResource(R.color.teal_700),
+                scale = true
+            )
+        }
     }
 }
