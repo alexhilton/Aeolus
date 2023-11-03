596dc9127e43313346a29c8486971ca2b29eb38d
Alex Hilton <dragonhilton@gmail.com>
2023-11-03 18:46:15 +0800

    Add reactive API for weather now repo.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/data/weather/WeatherNowRepository.kt
+++ app/src/main/java/net/toughcoder/aeolus/data/weather/WeatherNowRepository.kt
@@ -1,5 +1,10 @@
 package net.toughcoder.aeolus.data.weather
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.update
 import net.toughcoder.aeolus.data.WeatherLocation
 import net.toughcoder.aeolus.data.local.LocalDataSource
 
@@ -7,6 +12,8 @@ class WeatherNowRepository(
     private val local: LocalDataSource,
     private val network: WeatherNowDataSource
 ) {
+    private lateinit var stream: MutableStateFlow<WeatherNow>
+
     suspend fun getWeatherNow(location: WeatherLocation): WeatherNow {
         return local.loadWeatherNow(location)
     }
@@ -18,5 +25,19 @@ class WeatherNowRepository(
             local.updateWeatherNow(location, bundle)
         }
         return bundle
+    }
+
+    suspend fun weatherNowStream(location: WeatherLocation): Flow<WeatherNow> {
+        val localNow = local.loadWeatherNow(location)
+        stream = MutableStateFlow(localNow)
+        return stream.asStateFlow()
+    }
+
+    suspend fun refreshWeatherNow(location: WeatherLocation) {
+        val bundle = network.loadWeatherNow(location)
+        if (bundle.successful) {
+            local.updateWeatherNow(location, bundle)
+        }
+        stream.update { bundle }
     }
 }
