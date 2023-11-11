5e04a82c75b513288e5b3ffe120b93159df415bc
Alex Hilton <dragonhilton@gmail.com>
2023-11-11 14:12:31 +0800

    Use specific name again.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/net/toughcoder/aeolus/ui/favorites/FavoritesScreen.kt
@@ -1,4 +1,4 @@
-package net.toughcoder.aeolus.ui.location
+package net.toughcoder.aeolus.ui.favorites
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
