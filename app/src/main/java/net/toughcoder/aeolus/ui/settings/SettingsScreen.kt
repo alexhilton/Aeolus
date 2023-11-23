62022de7e64fd6762eb96bf7addd6ec8b96903d1
Alex Hilton <dragonhilton@gmail.com>
2023-11-23 22:54:11 +0800

    Simply show the settings entries.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/ui/settings/SettingsScreen.kt
+++ app/src/main/java/net/toughcoder/aeolus/ui/settings/SettingsScreen.kt
@@ -1,20 +1,30 @@
 package net.toughcoder.aeolus.ui.settings
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import net.toughcoder.aeolus.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,6 +34,8 @@ fun SettingsScreen(
     viewModel: SettingsViewModel,
     onBack: () -> Unit
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -50,6 +62,68 @@ fun SettingsScreen(
             )
         }
     ) {
-        Text(modifier = Modifier.padding(it), text = "Settings goes here!")
+        SettingsSection(Modifier.padding(it), uiState)
+    }
+}
+
+@Composable
+fun SettingsSection(
+    modifier: Modifier = Modifier,
+    uiState: SettingsUiState
+) {
+    Column(
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        if (uiState.language != null) {
+            SettingsEntry(
+                modifier,
+                uiState.language
+            )
+        }
+        if (uiState.unit != null) {
+            SettingsEntry(
+                modifier,
+                uiState.unit
+            )
+        }
+    }
+}
+
+@Composable
+fun SettingsEntry(
+    modifier: Modifier = Modifier,
+    entry: SettingsEntryUiState
+) {
+    Surface(
+        shape = MaterialTheme.shapes.small,
+        color = MaterialTheme.colorScheme.secondaryContainer,
+        shadowElevation = 6.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(entry.title),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Row {
+                Text(
+                    text = entry.value,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = ""
+                )
+            }
+        }
     }
 }
--- app/src/main/java/net/toughcoder/aeolus/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/net/toughcoder/aeolus/ui/settings/SettingsViewModel.kt
@@ -1,12 +1,50 @@
 package net.toughcoder.aeolus.ui.settings
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import net.toughcoder.aeolus.R
 import net.toughcoder.aeolus.data.AeolusStore
 
 class SettingsViewModel(
     store: AeolusStore
 ) : ViewModel() {
+    private val viewModelState = MutableStateFlow(SettingsUiState())
+
+    val uiState = viewModelState
+        .stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            viewModelState.value
+        )
+
+    init {
+        val lang = SettingsEntryUiState(
+            key = "language",
+            title = R.string.settings_entry_language_title,
+            value = "English",
+            options = listOf("English", "Chinese", "Auto")
+        )
+        val unit = SettingsEntryUiState(
+            key = "unit",
+            title = R.string.settings_entry_unit_title,
+            value = "Metric",
+            options = listOf("Metric", "Imperial")
+        )
+        viewModelState.update { SettingsUiState(lang, unit) }
+    }
+
+    fun updateSettingsEntry(entry: SettingsEntryUiState) {
+        val lang = if (entry.key == "language") entry else viewModelState.value.language
+        val unit = if (entry.key == "unit") entry else viewModelState.value.unit
+        viewModelState.update { SettingsUiState(lang, unit) }
+    }
+
     companion object {
         fun providerFactory(prefStore: AeolusStore) : ViewModelProvider.Factory =
             object : ViewModelProvider.Factory {
@@ -17,3 +55,15 @@ class SettingsViewModel(
             }
     }
 }
+
+data class SettingsUiState(
+    val language: SettingsEntryUiState? = null,
+    val unit: SettingsEntryUiState? = null
+)
+
+data class SettingsEntryUiState(
+    val key: String,
+    @StringRes val title: Int,
+    val value: String,
+    val options: List<String>
+)
