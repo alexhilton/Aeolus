086a2a795dfe0a3cd40b2b39bec6000c5e9f5eb4
Alex Hilton <dragonhilton@gmail.com>
2023-11-26 17:27:42 +0800

    Minor refactor.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/MainActivity.kt
+++ app/src/main/java/net/toughcoder/aeolus/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import net.toughcoder.aeolus.ui.theme.AeolusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +12,6 @@ class MainActivity : ComponentActivity() {
         val dataContainer = (application as AeolusAndroidApp).dataContainer
         setContent {
             AeolusApp(dataContainer, Modifier.fillMaxWidth())
-//            PullRefresh()
         }
     }
 }
