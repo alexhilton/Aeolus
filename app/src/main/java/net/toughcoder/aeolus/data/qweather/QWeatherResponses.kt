9070697ff41666f79a6e48f3fe3905527739baa4
Alex Hilton <dragonhilton@gmail.com>
2023-11-30 16:59:12 +0800

    Minor refactor.
    Move objects to its own domain.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/data/qweather/QWeatherResponses.kt
+++ app/src/main/java/net/toughcoder/aeolus/data/qweather/QWeatherResponses.kt
@@ -1,9 +1,6 @@
 package net.toughcoder.aeolus.data.qweather
 
 import com.google.gson.annotations.SerializedName
-import net.toughcoder.aeolus.model.DailyWeather
-import net.toughcoder.aeolus.model.Measure
-import retrofit2.http.Field
 
 data class QWeatherNowResponse(
     @field:SerializedName("code") val code: String,
@@ -24,28 +21,6 @@ data class QWeatherNowDTO(
     @field:SerializedName("vis") val visibility: String,
     @field:SerializedName("pressure") val pressure: String,
     @field:SerializedName("cloud") val cloud: String
-)
-
-data class QWeatherTopCitiesResponse(
-    @field:SerializedName("code") val code: String,
-    @field:SerializedName("topCityList") val topCityList: List<QWeatherCityDTO>
-)
-
-data class QWeatherCityDTO(
-    @field:SerializedName("name") val name: String,
-    @field:SerializedName("id") val qweatherId: String,
-    @field:SerializedName("lat") val latitude: String,
-    @field:SerializedName("lon") val longitude: String,
-    @field:SerializedName("adm2") val admin2: String,
-    @field:SerializedName("adm1") val admin1: String,
-    @field:SerializedName("country") val country: String,
-    @field:SerializedName("type") val type: String,
-    @field:SerializedName("rank") val rank: Int
-)
-
-data class QWeatherSearchResponse(
-    @field:SerializedName("code") val code: String,
-    @field:SerializedName("location") val cityList: List<QWeatherCityDTO>
 )
 
 data class QWeatherDailyResponse(
