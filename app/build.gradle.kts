00dcb184ca1598bf4d56957fd1afb74b845b37cb
Alex Hilton <dragonhilton@gmail.com>
2023-12-29 17:14:00 +0800

    Back to Giraffe.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,11 +66,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    val navVersion = "2.7.5"
+    implementation("androidx.activity:activity-compose:1.8.2")
+    val navVersion = "2.7.6"
     implementation("androidx.navigation:navigation-runtime-ktx:$navVersion")
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Google Play Services
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
