c2c2b35fe24eb9b8f1591ee63ca1e3e3e58fb565
Alex Hilton <dragonhilton@gmail.com>
2024-01-30 19:16:02 +0800

    Request location if there is no cached locations.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/aeolus/data/location/AndroidLocationClient.kt
+++ app/src/main/java/net/toughcoder/aeolus/data/location/AndroidLocationClient.kt
@@ -4,16 +4,25 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import android.location.LocationManager
+import android.location.LocationRequest
 import android.os.Build
 import androidx.annotation.RequiresApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
+import java.util.concurrent.Executor
 
 class AndroidLocationClient(
     context: Context
 ) : LocationProvider(context) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @RequiresApi(Build.VERSION_CODES.S)
     @SuppressLint("MissingPermission")
     override fun getLocation(): Flow<MyLocation> = flow {
@@ -41,11 +50,59 @@ class AndroidLocationClient(
         if (fromPassive != null) {
             locations.add(fromPassive)
         }
-        if (locations.isEmpty()) {
-            emit(emptyLocation(ERROR_NO_LOCATION))
-        } else {
+        if (locations.isNotEmpty()) {
             locations.sortByDescending { it.time }
             emit(MyLocation(locations[0].latitude, locations[0].longitude))
+        } else {
+            val builder = LocationRequest.Builder(500)
+                .setDurationMillis(60 * 1000) // Stop requesting after 1 min.
+
+            flowOf(LocationManager.GPS_PROVIDER, LocationManager.NETWORK_PROVIDER, LocationManager.FUSED_PROVIDER)
+                .flatMapLatest { current(it, locationManager, builder.build(), context.mainExecutor) }
+                .collect {
+                    emit(MyLocation(it.latitude, it.longitude))
+                }
         }
     }.flowOn(Dispatchers.IO)
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    @SuppressLint("MissingPermission")
+    private fun lastKnown(manager: LocationManager): Flow<Location> = flow {
+        val locations = mutableListOf<Location>()
+        val fromGps = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (fromGps != null) {
+            locations.add(fromGps)
+        }
+        val fromNetwork = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        if (fromNetwork != null) {
+            locations.add(fromNetwork)
+        }
+        val fromFused = manager.getLastKnownLocation(LocationManager.FUSED_PROVIDER)
+        if (fromFused != null) {
+            locations.add(fromFused)
+        }
+        val fromPassive = manager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER)
+        if (fromPassive != null) {
+            locations.add(fromPassive)
+        }
+        if (locations.isNotEmpty()) {
+            locations.sortByDescending { it.time }
+            emit(locations[0])
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun current(
+        provider: String,
+        manager: LocationManager,
+        request: LocationRequest,
+        executor: Executor
+    ): Flow<Location> = callbackFlow {
+        manager.getCurrentLocation(provider, request, null, executor) { loc ->
+            loc?.let { trySend(it) }
+        }
+
+        awaitClose {}
+    }
 }
