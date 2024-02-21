c81bcb5b1b585e2b173cac0b76af27b15708d8cc
Alex Hilton <dragonhilton@gmail.com>
2024-02-21 09:51:13 +0800

    Upgrade Compose BOM to 2024.02.00.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.1.0")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
