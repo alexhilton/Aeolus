ea51ff4ae7b521a19f14ed6985febcb10eef54d0
Alex Hilton <dragonhilton@gmail.com>
2024-11-26 18:02:22 +0800

    Upgrade Gradle and dependencies.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "net.toughcoder.aeolus"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "net.toughcoder.aeolus"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -60,20 +60,20 @@ dependencies {
     implementation(project(":qweather"))
 
     // Jetpack
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    val lifecycleVersion = "2.7.0"
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    val lifecycleVersion = "2.8.7"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    val navVersion = "2.7.7"
+    implementation("androidx.activity:activity-compose:1.9.3")
+    val navVersion = "2.8.4"
     implementation("androidx.navigation:navigation-runtime-ktx:$navVersion")
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Google Play Services
-    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
@@ -101,8 +101,8 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
